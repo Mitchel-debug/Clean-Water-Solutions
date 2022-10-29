a331ae85d98a15e26f7d5a186a9827671ccde4f7
Mitchel-debug <mitchelelusanmi1@gmail.com>
2022-10-29 16:35:50 -0400

    added script.js
    Added script.js to handle the Donation form

--- projects/Waterways/cleanWater/urls.py
+++ projects/Waterways/cleanWater/urls.py
@@ -3,7 +3,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("charge/", views.charge, name="charge"),
+    path("success/<str:args>/", views.successMsg, name="successMsg")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- projects/Waterways/cleanWater/views.py
+++ projects/Waterways/cleanWater/views.py
@@ -1,7 +1,44 @@
+import email
+from locale import currency
+from django.shortcuts import redirect, render
 from django.shortcuts import render
-from django.shortcuts import render
+from django.urls import reverse
+from urllib3 import HTTPResponse
 import cleanWater
+from django.http import HttpResponseRedirect, JsonResponse
+import stripe
 
-# Create your views here.
+stripe.api_key = "sk_test_51LyJ17BSE8CSa3qNLumX2XXGcoidGdc3sypq7xVE8sLhD2e8Oa7NAweBoNhDQdyVYHzdTWdDjmsvzuhXUhurgtvV008Np5GhjP" 
+
 def index(request):
-    return render(request, "cleanWater/index.html")
+    list = (0, 1, 2)
+    return render(request, "cleanWater/index.html", {
+        "List": list
+    })
+    
+def charge(request):
+    amount = 5
+    if request.method == 'POST':
+        print("Data: ", request.POST)
+        
+        customer = stripe.Customer.create(
+            email = request.POST['email'],
+            name = request.POST['nickname'],
+            source = request.POST['stripeToken']
+        )
+        
+        charge = stripe.Charge.create(
+            customer=customer,
+            amount=500,
+            currency="usd",
+            description="Donation",
+            
+        )
+        
+    return HttpResponseRedirect(reverse("successMsg", args=[amount]))
+
+def successMsg(request, args):
+    amount = args
+    return render(request, "cleanWater/success.html", {
+        "amount": amount
+    })
